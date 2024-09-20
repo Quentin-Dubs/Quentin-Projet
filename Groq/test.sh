67bf41d9950c0a70d51026456acd021327ed6c6e
Quentin-Dubs <dubois9164@gmail.com>
2024-09-20 16:11:52 +0200

    3ème commit

--- Groq/test.sh
+++ Groq/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X POST http://localhost:8000/docs#/default/chat -H "Content-Type: application/json" -d '{"prompt":"What is a LLM?"}'
+curl -X POST http://localhost:8000/chat -H "Content-Type: application/json" -d '{"prompt":"What is a LLM?"}'
